2dfaaf8b896c09c4618c01136417a5c701d8293f
Kofo <kajotonis@gmail.com>
2022-04-12 22:51:29 +0100

    feature

---
+++ nestlibrary_react/my-project/src/App.js
@@ -0,0 +1,20 @@
+import {Navbar, Body} from './components'
+import {Home, Upload} from './pages'
+
+const App = () => {
+
+  return (
+    <div className="mt-10 ">
+    <div>
+      
+    </div>
+
+    <main>
+     <Home />
+     <Upload />
+    </main>
+    </div>
+  )
+}
+
+export default App
